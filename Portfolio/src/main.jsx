3303989a280be5dcd3e1f57398da958b9b7c7af7
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-14 11:35:06 -0600

    building main router

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+
+const router = createBrowserRouter (
+  [
+    {
+      path: '/',
+      element: <App />
+      error: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />
+        },
+        {
+          index: true,
+          element: <ContactPage />
+        },
+        {
+          index: true,
+          element: <AboutPage />
+        }
+      ]
+    }
+  ]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
